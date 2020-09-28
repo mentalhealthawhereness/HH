80dac4583c62bbdaeb04703ba047b2a5daab82dc
mentalhealthawhereness <38903439+mentalhealthawhereness@users.noreply.github.com>
2020-09-28 22:16:36 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -73,8 +73,8 @@ window.onload = function () {
 
     //// load map ////////
 
-    var lat = 25.032;
-    var lng = 121.53;
+    var lat = 12.85985;
+    var lng = 114.03808;
     var zoom = 13;
 
     var mymap = L.map('mapid').setView([lat, lng], zoom);
